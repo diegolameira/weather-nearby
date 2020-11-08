57edd44baec3919bbe60e5e44cff288b47f5d33e
Diego Lameira <diegolameira@me.com>
2020-11-08 16:16:12 -0300

    feat(map): map user interactions

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,8 +1,17 @@
-import * as React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import * as React from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  StandaloneSearchBox,
+  Marker,
+} from '@react-google-maps/api';
+
+import styles from 'styles/components.module.scss';
 
 export const Map: React.FC = () => {
-  const [mapProps] = React.useState({
+  const searchRef = React.useRef<any>();
+  const [state, setState] = React.useState({
+    position: {},
     mapContainerStyle: {
       width: '100vw',
       height: '100vh',
@@ -11,15 +20,33 @@ export const Map: React.FC = () => {
       lat: -22,
       lng: -43,
     },
-    zoom: 10,
-  })
+    zoom: 6,
+  });
+
+  const handleClick = ({ latLng }: any) => {
+    setState((currentState) => ({ ...currentState, position: latLng, center: latLng }));
+  }
+
+  const handlePlacesChange = () => {
+    const { lat, lng } = searchRef.current?.getPlaces()[0].geometry.location;
+    const position = { lat: lat(), lng: lng() }
+    setState((currentState) => ({ ...currentState, position, center: position }));
+  };
 
   return (
     <LoadScript
       googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}
       libraries={['places']}
-      >
-      <GoogleMap {...mapProps} />
+    >
+      <GoogleMap onClick={handleClick} {...state}>
+        <StandaloneSearchBox
+          onLoad={(ref) => (searchRef.current = ref)}
+          onPlacesChanged={handlePlacesChange}
+        >
+          <input type="search" placeholder="Find a city or a place" className={styles.mapSearchBox} />
+        </StandaloneSearchBox>
+        <Marker position={state.position} />
+      </GoogleMap>
     </LoadScript>
-  )
-}
+  );
+};
