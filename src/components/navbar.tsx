24dc56a5e66cb6a24f6b769169dbfafd38d89c8c
Diego Lameira <diegolameira@me.com>
2020-11-08 15:20:00 -0300

    feat(navigation): navbar component 1. add sass to use module.scss 2. add icons svg to new assets directory 3. develop navbar responsive component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,42 @@
 import * as React from 'react';
-import { Link } from "@reach/router";
+import { Link } from '@reach/router';
+
+import styles from 'styles/components.module.scss';
+
+import { ReactComponent as MapIcon } from 'assets/map.svg';
+import { ReactComponent as MapIconActive } from 'assets/map-active.svg';
+import { ReactComponent as SearchIcon } from 'assets/search.svg';
+import { ReactComponent as SearchIconActive } from 'assets/search-active.svg';
+import { ReactComponent as ListIcon } from 'assets/list.svg';
+import { ReactComponent as ListIconActive } from 'assets/list-active.svg';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav>
-      <Link to={'/'}>
-        Map
-      </Link>
-      <Link to={'/search'}>
-        Search
-      </Link>
-      <Link to={'/list'}>
-        List
-      </Link>
+    <nav className={styles.nav}>
+      <Link
+        to={'/'}
+        getProps={({ isCurrent }) => {
+          return {
+            children: isCurrent ? <MapIconActive /> : <MapIcon />,
+          };
+        }}
+      />
+      <Link
+        to={'/search'}
+        getProps={({ isCurrent }) => {
+          return {
+            children: isCurrent ? <SearchIconActive /> : <SearchIcon />,
+          };
+        }}
+      />
+      <Link
+        to={'/list'}
+        getProps={({ isCurrent }) => {
+          return {
+            children: isCurrent ? <ListIconActive /> : <ListIcon />,
+          };
+        }}
+      />
     </nav>
   );
-}
+};
