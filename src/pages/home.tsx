1b98798aec1940612ab69fc33ce28cdb17b78d92
Diego Lameira <diegolameira@me.com>
2020-11-08 23:20:45 -0300

    some latest adjustments

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { Router, RouteComponentProps } from "@reach/router"
 
+import { ReactComponent as Logo } from 'assets/logo.svg'
+
 import styles from 'styles/pages.module.scss'
 import { Map } from 'components/map'
 import { ListScreen } from './list';
@@ -10,6 +12,7 @@ interface Props extends RouteComponentProps {}
 export const HomeScreen: React.FC<Props> = () => {
   return (
     <div className={styles.homeScreen}>
+      <Logo className={styles.logo} />
       <Map />
       <Router>
         <SearchScreen path="/search" />
